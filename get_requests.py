ac458746a5c5d7ae8559c858ca2fc61e308bab31
a-stephen <sadjignon015@gmail.com>
2022-06-15 17:03:12 +0000

    add tag extractor: initial commit

--- get_requests.py
+++ get_requests.py
@@ -1,8 +1,12 @@
+from genericpath import exists
+import os
+import sys
+import re
 import requests
 
 # make a request using get
 
-r = requests.get("https://httpbin.org")
+r = requests.get("https://www.w3schools.com")
 
 # print(r.url) # return the passed url
 # print("==============")
@@ -10,7 +14,7 @@ r = requests.get("https://httpbin.org")
 # print("==============")
 # print(r.content) # Returns the raw html
 # print("==============")
-# print(r.headers["content-type"]) # returns 
+# print(r.headers["content-type"]) # returns headers: content-type
 # print("==============")
 # print(r.text) # returns a pretty formatted of content
 # print("==============")
@@ -26,11 +30,28 @@ def write_rcontent(httpObject):
     """
         Returns a .txt file populated with the content of the httpObject.
     """
-    with open('content.txt', 'a') as content:
-        content.write(httpObject.text)
+    if 'content.txt' not in os.listdir():
+        with open('content.txt', 'a') as content:
+            content.write(httpObject.text)
+    else: print("the file already exists")
+    return
 
-    return content
+def extract_tags(tag, httpObject):
+    """
+        return the number of tag in the http response
 
+            tag: string (exple: a, link, p, br)
 
+            if return odd number the tag has only an open-tag else the tag has both the open-tag and close-tag
+    """
+    
+    return len(re.findall(tag, httpObject.text)) + 1
 
 ab = write_rcontent(r)
+
+# does not work quiet well yet, can be fixed....
+ra = extract_tags("html", r)
+
+print(ra)
+
+# print(type(r.text))
